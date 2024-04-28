4bb7496aee1088909db9f55940b7766d97783bce
VivTravail <vivtravail@Viveks-MacBook-Pro-2.local>
2024-04-28 17:15:42 -0400

    enhancement and refactoring

--- src/components/Currency-Convertor/CurrencyConvertor.jsx
+++ src/components/Currency-Convertor/CurrencyConvertor.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { IoMdSwap } from "react-icons/io";
+import { HiArrowsRightLeft } from "react-icons/hi2";
+import CurrencyDropdown from "./CurrencyDropdown";
 const CurrencyConvertor = () => {
   //states
   const [currencies, setCurrencies] = useState([]);
@@ -8,13 +9,15 @@ const CurrencyConvertor = () => {
   const [fromCurrency, setFromCurrency] = useState("CAD");
   const [toCurrency, setToCurrency] = useState("INR");
   const [converting, setConverting] = useState(false);
+  const [favorites, setFavorites] = useState(
+    JSON.parse(localStorage.getItem("favorites")) || ["CAD", "USD", "INR"]
+  );
   // Currencies -> https://api.frankfurter.app/currencies
   const fetchCurrencies = async () => {
     try {
       const res = await fetch("https://api.frankfurter.app/currencies");
       const data = await res.json();
-
-      console.log(data);
+      setCurrencies(Object.keys(data));
     } catch (error) {
       console.error("Error Fetching", error);
     }
@@ -36,38 +39,69 @@ const CurrencyConvertor = () => {
       setConverting(false);
     }
   };
+  const handleSwap = () => {
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
+  };
+  const handleFavorite = (currency) => {
+    let updatedFavorites = [...favorites];
+    if (favorites.includes(currency))
+      updatedFavorites = updatedFavorites.filter((fav) => fav !== currency);
+    else updatedFavorites.push(currency);
+    setFavorites(updatedFavorites);
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+  };
   useEffect(() => {
     fetchCurrencies();
   }, []);
   return (
-    <div className="max-w-xl mx-auto my-10 p-5 bg-white rounded-lg shadow-md">
+    <div className="max-w-2xl h-70 mx-auto my-10 p-5 bg-white rounded-lg shadow-md">
       <h2 className="mb-5 text-2xl font-semibold text-center text-gray-700">
         Currency Convertor
       </h2>
-      <div className="flex gap-9">
-        <div className="border-4 border-blue-500 rounded-sm shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-1">
-          <input type="number" value={amount}></input>
-          <label>CAD</label>
+      <div className="flex gap-8">
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <input
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            type="text"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <CurrencyDropdown
+            currencies={currencies}
+            currency={fromCurrency}
+            setCurrency={setFromCurrency}
+            handleFavorite={handleFavorite}
+            favorites={favorites}
+          />
         </div>
-        <IoMdSwap className="w-35 h-35 mt-3" />
-        <div
-          className="
-          border-4
-          border-blue-500
-          rounded-sm
-          shadow-sm
-          focus:outline-none
-          focus:ring-2
-          focus:ring-indigo-500
-          mt-1"
+        <button
+          onClick={handleSwap}
+          className="mt-1 p-3 bg-gray-300 rounded-full cursor-pointer hover:bg-gray-500"
         >
-          <input type="number" value={convertedAmount}></input>
-          <label>INR</label>
+          <HiArrowsRightLeft className="text-xl text-gray-800" />
+        </button>
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <input
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            type="text"
+            value={convertedAmount}
+            onChange={(e) => setConvertedAmount(e.target.value)}
+          />
+          <CurrencyDropdown
+            favorites={favorites}
+            currencies={currencies}
+            currency={toCurrency}
+            setCurrency={setToCurrency}
+            handleFavorite={handleFavorite}
+          />
         </div>
       </div>
       <div
         onClick={convertCurrency}
-        className="px-5 py-2 mt-7 text-center bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        className={`px-5 py-2 mt-12 text-center bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ${
+          converting ? "animate-pulse" : ""
+        }`}
       >
         Convert
       </div>
---
+++ src/components/Currency-Convertor/CurrencyDropdown.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { HiOutlineStar, HiStar } from "react-icons/hi2";
+
+const CurrencyDropdown = ({
+  currencies,
+  currency,
+  setCurrency,
+  favorites,
+  handleFavorite,
+}) => {
+  const isFavorite = (curr) => favorites.includes(curr);
+  return (
+    <div className="absolute inset-y-0 right-0 flex items-center">
+      <select
+        className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm"
+        value={currency}
+        onChange={(e) => setCurrency(e.target.value)}
+      >
+        {favorites.map((currency) => (
+          <option value={currency} key={currency}>
+            {currency}
+          </option>
+        ))}
+        <hr />
+        {currencies
+          ?.filter((curr) => !favorites.includes(curr))
+          .map((currency) => (
+            <option value={currency} key={currency}>
+              {currency}
+            </option>
+          ))}
+      </select>
+      <button className="pr-6" onClick={() => handleFavorite(currency)}>
+        {isFavorite(currency) ? <HiStar /> : <HiOutlineStar />}
+      </button>
+    </div>
+  );
+};
+
+export default CurrencyDropdown;
