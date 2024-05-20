9e0c6a45c9314d90c2ee875b75dc162e531635bb
VivTravail <vivtravail@Viveks-MacBook-Pro-2.local>
2024-05-19 22:39:51 -0400

    breadcrumb part

--- src/components/BreadCrumb/BreadCrumb.jsx
+++ src/components/BreadCrumb/BreadCrumb.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const BreadCrumb = () => {
-  return <div>BreadCrumb</div>;
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter((x) => x);
+  let breadcrumbPath = "";
+  console.log(pathnames);
+  return (
+    <div className="breadcrumbs">
+      <Link to="/">Home</Link>
+      {pathnames.map((name, index) => {
+        breadcrumbPath += `/${name}`;
+        const isLast = index === pathnames.length - 1;
+        return isLast ? (
+          <span key={breadcrumbPath}>/ {name}</span>
+        ) : (
+          <span key={breadcrumbPath}>
+            {" "}
+            /<Link to={breadcrumbPath}>{name}</Link>
+          </span>
+        );
+      })}
+    </div>
+  );
 };
 
 export default BreadCrumb;
